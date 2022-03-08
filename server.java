9408b178615e820deef7bc57ec2a417bb38e549f
Snipez9492 <waleibk92@gmail.com>
2022-03-08 18:10:18 -0500

    Next Commit

--- server.java
+++ server.java
@@ -1,6 +1,22 @@
+import java.io.*;
+import java.net.*;
 
 public class server {
+
     public static void main(String[] args) {
-        System.out.println("This is HW3");
+        try {
+            ServerSocket server = new ServerSocket(3001);
+            Socket now = server.accept();
+
+            DataInputStream input = new DataInputStream(now.getInputStream());
+
+            String str = (String) input.readUTF();
+            System.out.println("message= " + str);
+
+            server.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
     }
 }
